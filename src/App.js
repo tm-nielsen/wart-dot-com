20c7d7d423c4310a88eec5f91616a51ba33b40c2
Taylor Nielsen <littleb88888888@gmail.com>
2023-09-15 15:14:20 -0600

    Added basic routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-function App() {
+import Home from "./routes/Home";
+import Admin from "./routes/Admin";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+            <Route exact path="/" element={<Home/>} />
+            <Route path="/admin" element={<Admin/>} />
+            <Route path="*" element={<h1>Error: Page does not exist</h1>} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
