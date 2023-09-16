444c9d3e6a72c0045aae5ce0fdacc2c26142b426
Taylor Nielsen <littleb88888888@gmail.com>
2023-09-16 03:26:05 -0600

    Added use fetch to home for current prompt

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import useFetch from '../hooks/useFetch'
 import WordList from '../components/WordList'
 import SuggestionBox from '../components/SuggestionBox'
 import Footer from '../components/Footer'
@@ -6,16 +7,30 @@ import Footer from '../components/Footer'
 const serverUrl = 'http://localhost:3001'
 
 const Home = () => {
+  const [currentPrompt, setCurrentPrompt] = useState('')
+  const [fetchData, error, setError] = useFetch()
+
+
+  useEffect(() => {
+    fetchData('active', handleResponse)
+  }, [])
+
+  const handleResponse = (data) => {
+    setCurrentPrompt(data)
+  }
+
+
   return (
     <>
       <h1 className="acc">This Week's WArt word is:</h1>
-      <p id="current-prompt">fast</p>
+      <p id="current-prompt">{currentPrompt}</p>
       <div className="spacer"/>
       <SuggestionBox/>
       <div className="spacer"/>
       <WordList title="Prompt Pool" category="current" serverUrl={serverUrl} />
       <WordList title="Past Prompts" category="past" serverUrl={serverUrl} />
       <WordList title="Pending Approval" category="pending" serverUrl={serverUrl} />
+      <WordList title="Test" category="nothing" serverUrl={serverUrl} />
       <div className="spacer"/>
       <Footer/>
     </>
