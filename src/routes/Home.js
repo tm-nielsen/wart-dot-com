579606c92c2a596bad7d407f1cbaa270be8959c3
Taylor Nielsen <littleb88888888@gmail.com>
2023-09-15 20:25:40 -0600

    Added spacers

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -7,15 +7,16 @@ const Home = () => {
     <>
       <h1 className="acc">This Week's WArt word is:</h1>
       <p id="current-prompt">fast</p>
+      <div className="spacer"/>
       <form action="">
         <label htmlFor="prompt">Suggest a Prompt: </label>
         <input type="text" name="prompt" id="prompt" />
       </form>
-      
+      <div className="spacer"/>
       <WordList title="Prompt Pool" category="current" />
       <WordList title="Past Prompts" category="past" />
-      <WordList title="Suggestions Pending Approval" category="pending" />
-      
+      <WordList title="Pending Approval" category="pending" />
+      <div className="spacer"/>
       <Footer/>
     </>
   )
