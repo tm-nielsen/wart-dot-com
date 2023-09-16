67744f362eed6a5b14a2cf6d5abb91a94e525dee
Taylor Nielsen <littleb88888888@gmail.com>
2023-09-15 18:38:23 -0600

    Added footer + styling

---
+++ src/components/Footer.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import "../styles/footer.css"
+
+import clubIcon from "../assets/TransparentClubIcon.png"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="flex-row">
+        <h3 className="acc">
+          WArt is a University of Calgary<br/>
+          Game Design Club activity
+        </h3>
+        <a href="https://linktr.ee/gamedesignclub" target = "_blank"
+            className="link acc-border shadow" id="club-link">
+          <img src={clubIcon} alt="Discord Icon" id="club-icon"/>
+        </a>
+      </div>
+
+      <ol className="flex-row flat">
+        <a href="https://discord.gg/XKU6wHnCYD" target = "_blank"
+            className="link acc-border shadow">
+          Discord
+        </a>
+        <a href="https://www.instagram.com/uofcgamedesign/" target = "_blank"
+            className="link acc-border shadow">
+          Instagram
+        </a>
+        <a href="https://uofcgamedesignclub.itch.io/" target = "_blank"
+            className="link acc-border shadow">
+          Itch.io
+        </a>
+      </ol>
+    </div>
+  )
+}
+
+export default Footer
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,11 @@
 import React from 'react'
-
-import clubIcon from "../assets/ClubIcon.png"
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <>
       <h1 className="acc">This Week's WArt word is:</h1>
-      <p className="current-prompt">fast</p>
+      <p id="current-prompt">fast</p>
       <form action="">
         <label htmlFor="prompt">Suggest a Prompt: </label>
         <input type="text" name="prompt" id="prompt" />
@@ -23,12 +22,7 @@ const Home = () => {
       <ol>
         {Array.from({length: 20}, (x, i) => i).map((i) => <p>word {i}</p>)}
       </ol>
-      <div>
-        <h1>Club Info Footer</h1>
-        <a href="">
-          <img src={clubIcon} alt="Game Design Club icon" className="footer-img" />
-        </a>
-      </div>
+      <Footer/>
     </>
   )
 }
