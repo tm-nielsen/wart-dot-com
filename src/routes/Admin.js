b505c49cdc36ebd3c6320dbabbc193204a78c887
Taylor Nielsen <littleb88888888@gmail.com>
2023-09-17 02:39:35 -0600

    Sketched admin page structure

--- src/routes/Admin.js
+++ src/routes/Admin.js
@@ -3,11 +3,14 @@ import { Link } from 'react-router-dom'
 import useFetch from '../hooks/useFetch'
 import Login from '../components/admin/Login'
 
+
 const Admin = ({serverUrl}) => {
   const {get, post, patch} = useFetch(serverUrl)
   const [password, setPassword] = useState('')
   const [showWrong, setShowWrong] = useState(false)
   const [authenticated, setAuthenticated] = useState(false)
+  const [currentPage, setCurrentPage] = useState(0)
+
 
   const onLoginSubmit = (value) => {
     setPassword(value)
@@ -18,17 +21,54 @@ const Admin = ({serverUrl}) => {
     })
   }
   
-  // primary (each own page)
-  // select new prompt
-  // approve/reject pending
+  // 4 pages
+  // main with buttons
 
-  // secondary (all one page)
-  // insert prompt
-  // remove prompt
+  // approve/reject pending
+  // confirm selection of new prompt
+  // edit: insert/delete directly
 
   let pageContent
   if (authenticated) {
-    pageContent = <div>Admin</div>
+    switch(currentPage){
+      case 0:
+        pageContent = (
+          <>
+            <button onClick={() => setCurrentPage(1)}>Approve Submissions</button>
+            <button onClick={() => setCurrentPage(2)}>Select New Prompt</button>
+            <button onClick={() => setCurrentPage(3)}>Edit</button>
+          </>
+        )
+        break
+
+      case 1:
+        pageContent = (
+          <>
+            <button onClick={() => setCurrentPage(0)}>Back</button>
+            <h1>Approve Pending Submissions</h1>
+          </>
+        )
+        break
+
+      case 2:
+        pageContent = (
+          <>
+            <button onClick={() => setCurrentPage(0)}>Back</button>
+            <h1>Select a New Active Prompt</h1>
+            <button onClick={() => console.log('selecting new prompt')}>Confirm</button>
+          </>
+        )
+        break
+
+      case 3:
+        pageContent = (
+          <>
+            <button onClick={() => setCurrentPage(0)}>Back</button>
+            <h1>Edit Prompt Lists</h1>
+          </>
+        )
+        break
+    }
   }
   else {
     pageContent = <Login onSubmit={onLoginSubmit} showWrong={showWrong} />
@@ -37,7 +77,7 @@ const Admin = ({serverUrl}) => {
   return (
     <>
       {pageContent}
-      <Link to='/'>Back</Link>
+      <Link to='/'>Home</Link>
     </>
   )
   
