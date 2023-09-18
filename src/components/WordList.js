36859217a6d1ecbec72e232cdb8794a094434179
Taylor Nielsen <littleb88888888@gmail.com>
2023-09-17 22:33:43 -0600

    Fixed wordlist conditional rendering

--- src/components/WordList.js
+++ src/components/WordList.js
@@ -2,15 +2,13 @@ import React from 'react'
 import '../styles/wordlist.css'
 
 const WordList = ({title, content}) => {
-  if (Array.isArray(content))
+  if (Array.isArray(content) && content.length > 0)
   return (
     <div id="word-list-container">
       <h2>{title}:</h2>
       <ol id="word-list">
-        {
-          Array.isArray(content)? 
-          content.map((word, index) => <p key={index} id="prompt" className='acc-faded'>{word}</p>)
-          :<p className='acc'>{content}</p>
+        {content.map((word, index) =>
+          <p key={index} id="prompt" className='acc-faded'>{word}</p>)
         }
       </ol>
     </div>
