2f323098021a9302dd544ca62072ef3c7c3bfee9
Taylor Nielsen <littleb88888888@gmail.com>
2023-09-18 12:51:44 -0600

    Disable confirm select button after use

--- src/routes/admin/ConfirmSelectPage.js
+++ src/routes/admin/ConfirmSelectPage.js
@@ -1,18 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { wrappedPatch } from '../../FetchMethods'
 import { useAuthContext } from '../../contexts/AuthContext'
 
 const ConfirmSelectPage = () => {
+  const [disabled, setDisabled] = useState(false)
   const {password} = useAuthContext()
 
   const confirmSelect = () => {
     wrappedPatch('select', {password})
+    setDisabled(true)
   }
 
   return (
     <>
       <h1>Select a New Active Prompt</h1>
-      <button onClick={confirmSelect}>Confirm</button>
+      <button onClick={confirmSelect} disabled={disabled}>Confirm</button>
     </>
   )
 }
