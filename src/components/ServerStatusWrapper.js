83d34b924190f70370bfc0eb5898b5a92763efb4
Taylor Nielsen <littleb88888888@gmail.com>
2023-09-19 16:15:13 -0600

    Updated server status wrapper

--- src/components/ServerStatusWrapper.js
+++ src/components/ServerStatusWrapper.js
@@ -1,17 +1,41 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { wrappedGet } from '../FetchMethods'
+
 
 const ServerStatusWrapper = ({children}) => {
   const [serverStatus, setServerStatus] = useState(false)
+  const [dotCount, setDotCount] = useState(0)
 
   useEffect(() => {
     wrappedGet('status', setServerStatus)
   }, [])
 
+  // https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+  const useInterval = (callback, delay) => {
+    const intervalRef = useRef()
+    const callbackRef = useRef(callback)
+
+    useEffect(() => {
+      callbackRef.current = callback
+    }, [callback])
+
+    useEffect(() => {
+      if (delay) {
+        intervalRef.current = setInterval(() => callbackRef.current(), delay)
+        return () => clearInterval(intervalRef.current)
+      }
+    }, [delay])
+
+    return intervalRef
+  }
+
+  useInterval(() => setDotCount((dotCount + 1) % 4), 600)
+
+
   if (serverStatus) return children
   else return (
     <div style={{height: '60vh'}}>
-      <h1>Server Offline</h1>
+      <h1>Connecting to Server{'.'.repeat(dotCount)}</h1>
     </div>
   )
 }
