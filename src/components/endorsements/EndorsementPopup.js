7535ea0914d92cbafbc7f19bde2053de5118879a
Taylor Nielsen <littleb88888888@gmail.com>
2023-09-20 15:37:00 -0600

    Improved endorse button style

--- src/components/endorsements/EndorsementPopup.js
+++ src/components/endorsements/EndorsementPopup.js
@@ -4,9 +4,9 @@ const EndorsementPopup = ({word, endorsements, endorsePrompt, disabled}) => {
 
   return (
     <div className='endorsement-popup'>
-      <label htmlFor='endorse-button' className='endorse-label'>
+      <h2 className='endorse-label'>
         {disabled? `${word} Endorsed`: `Endorse ${word}?`}
-      </label>
+      </h2>
       <div className="flex-row endorse-row">
         <p className='endorsement-count acc'>{endorsements}</p>
         <button id='endorse-button' onClick={endorsePrompt} disabled={disabled}>+1</button>
